3efbcd0c92d29d1299f5a8d8f51e808212ac7efb
William Walker <w_walker@icloud.com>
2024-06-01 12:15:31 -0500

    feat: basic functionality (#2)
    * reject unknown emails
    
    * added new test
    
    * basic API calls
    
    * add fallback outcome
    
    * use octokit
    
    * begin test refactor

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,65 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// src/index.ts
+import { Octokit } from "@octokit/core";
+
+async function streamToText(stream: ReadableStream<Uint8Array>): Promise<string> {
+  const reader = stream.getReader();
+  const decoder = new TextDecoder();
+  let result = '';
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+    result += decoder.decode(value, { stream: true });
+  }
+
+  result += decoder.decode();
+  return result;
+}
+
+async function createIssue(message: ForwardableEmailMessage, env: Env, octokit: Octokit): Promise<void> {
+  let messageTitle: string;
+  let messageBody: string;
+
+  try {
+    messageTitle = message.headers.get('subject') || "User feedback";
+    messageBody = await streamToText(message.raw);
+  } catch (error) {
+    throw new CreateIssueError("Unable to parse email message contents", error as any);
+  }
+
+  try {
+    await octokit.request('POST /repos/{owner}/{repo}/issues', {
+      owner: env.GITHUB_USERNAME,
+      repo: env.GITHUB_REPO,
+      title: messageTitle,
+      body: messageBody,
+      labels: [
+        'feedback'
+      ],
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log('Issue created successfully');
+  } catch (error) {
+    throw new CreateIssueError("Unable to create GitHub issue", error as any);
+  }
+}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
+    const user = env.GITHUB_USERNAME;
+    const repo = env.GITHUB_REPO;
+    const token = env.GITHUB_TOKEN;
+
+    if (!user || !repo || !token) {
+      throw new MissingEnvVariablesError();
+    }
+
+    const octokit = new Octokit({
+      auth: token
+    });
+
+    return createIssue(message, env, octokit);
+  }
 };
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -7,19 +7,43 @@ import worker from '../src/index';
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
+function stringToReadableStream(str: string): ReadableStream<Uint8Array> {
+	const encoder = new TextEncoder();
+	const uint8Array = encoder.encode(str);
+
+	return new ReadableStream({
+		start(controller) {
+		controller.enqueue(uint8Array);
+		controller.close();
+		}
+	});
+}
+
+let exampleEmail: ForwardableEmailMessage = {
+	from: "test@willswire.com",
+	to: "octomailer@afiexplorer.com",
+	raw: stringToReadableStream("Hello World!"),
+	headers: new Headers(),
+	rawSize: 10,
+	setReject(reason) {
+		console.log(`Rejected because ${reason}`)
+	},
+	forward(rcptTo, headers) {
+		console.log(`Forwarding to ${rcptTo}`)
+		return Promise.resolve();
+	},
+};
+
+describe('Octomailer worker', () => {
+	it('creates a GitHub issue', async () => {
+		// Create an empty context to pass to `worker.email()`.
 		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
+		const response = await worker.email(exampleEmail, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		// TODO: query GitHub API with issue number to verify the creation of an email
+		
+		// expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
 	});
 });
