9cbdcc9ebaeff14e6c8011f3cd5793f92ea21ea6
William Walker <w_walker@icloud.com>
2024-06-01 12:10:57 -0500

    begin test refactor

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,54 +1,49 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF, fetchMock } from 'cloudflare:test';
+import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
 
-enum OctomailerWorkerOutcome {
-	success,
-	failure,
-	rejected
+// For now, you'll need to do something like this to get a correctly-typed
+// `Request` to pass to `worker.fetch()`.
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
+function stringToReadableStream(str: string): ReadableStream<Uint8Array> {
+	const encoder = new TextEncoder();
+	const uint8Array = encoder.encode(str);
+
+	return new ReadableStream({
+		start(controller) {
+		controller.enqueue(uint8Array);
+		controller.close();
+		}
+	});
 }
 
-describe('Octomailer worker', () => {
-	it('rejects a bad email', async () => {
-		let badEmail: ForwardableEmailMessage = {
-			from: "test@willswire.com",
-			to: "badjuju@afiexplorer.com",
-			raw: new ReadableStream(),
-			headers: new Headers(),
-			rawSize: 10,
-			setReject(reason) {
-				console.log(`Rejected because ${reason}`)
-			},
-			forward(rcptTo, headers) {
-				console.log(`Forwarding to ${rcptTo}`)
-				return Promise.resolve();
-			},
-		};
-		const ctx = createExecutionContext();
-		const result = await worker.email(badEmail, env, ctx);
-		await waitOnExecutionContext(ctx);
-		expect(result).toMatchInlineSnapshot(OctomailerWorkerOutcome.rejected);
-	});
+let exampleEmail: ForwardableEmailMessage = {
+	from: "test@willswire.com",
+	to: "octomailer@afiexplorer.com",
+	raw: stringToReadableStream("Hello World!"),
+	headers: new Headers(),
+	rawSize: 10,
+	setReject(reason) {
+		console.log(`Rejected because ${reason}`)
+	},
+	forward(rcptTo, headers) {
+		console.log(`Forwarding to ${rcptTo}`)
+		return Promise.resolve();
+	},
+};
 
-	it('creates an issue from a good email', async () => {
-		let goodEmail: ForwardableEmailMessage = {
-			from: "test@willswire.com",
-			to: "feedback@afiexplorer.com",
-			raw: new ReadableStream(),
-			headers: new Headers(),
-			rawSize: 10,
-			setReject(reason) {
-				console.log(`Rejected because ${reason}`)
-			},
-			forward(rcptTo, headers) {
-				console.log(`Forwarding to ${rcptTo}`)
-				return Promise.resolve();
-			},
-		};
+describe('Octomailer worker', () => {
+	it('creates a GitHub issue', async () => {
+		// Create an empty context to pass to `worker.email()`.
 		const ctx = createExecutionContext();
-		const result = await worker.email(goodEmail, env, ctx);
+		const response = await worker.email(exampleEmail, env, ctx);
+		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(result).toMatchInlineSnapshot(OctomailerWorkerOutcome.success);
+
+		// TODO: query GitHub API with issue number to verify the creation of an email
+		
+		// expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
 	});
 });
